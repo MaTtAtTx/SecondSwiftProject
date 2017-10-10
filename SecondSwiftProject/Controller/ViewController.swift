4039d41d35dc6b036cca868a25df1749bd76ede7
mattitude17 <31513806+mattitude17@users.noreply.github.com>
2017-10-10 07:55:01 -0600

    Created the project, added GitIgnore, and created a button in the app that changes its color, and also the background color to a random color.

---
+++ SecondSwiftProject/Controller/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  SecondSwiftProject
+//
+//  Created by Johnsen, Matthew on 10/10/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+    @IBOutlet weak var firstButton: UIButton!
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func firstMethod(_ sender: UIButton)
+    {
+        if (firstButton.backgroundColor == .blue)
+        {
+            firstButton.backgroundColor = .purple
+        }
+        else
+        {
+            firstButton.backgroundColor = .blue
+        }
+        view.backgroundColor = createRandomColor()
+    }
+    
+    private func createRandomColor() -> UIColor
+    {
+        let newColor : UIColor
+        let redAmount = CGFloat (Double (arc4random_uniform(256))/255.00)
+        let greenAmount = CGFloat (Double (arc4random_uniform(256))/255.00)
+        let blueAmount = CGFloat (Double (arc4random_uniform(256))/255.00)
+        newColor = UIColor(red: redAmount, green: greenAmount, blue: blueAmount, alpha: CGFloat(1.0))
+        
+        return newColor
+    }
+    
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
