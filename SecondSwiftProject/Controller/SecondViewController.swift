2ee39492fa9c8a06726a1f3d9b31d3a97a0cc631
mattitude17 <31513806+mattitude17@users.noreply.github.com>
2017-10-12 09:14:59 -0600

    Created and linked a second screen and made it so buttons can go back and forth between screens.

---
+++ SecondSwiftProject/Controller/SecondViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SecondViewController.swift
+//  SecondSwiftProject
+//
+//  Created by Johnsen, Matthew on 10/12/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+import UIKit
+
+public class SecondViewController : UIViewController
+{
+    public override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
