350180835ac13de35caf88ddca3cf282c92bcb72
mattitude17 <31513806+mattitude17@users.noreply.github.com>
2017-10-12 09:35:20 -0600

    Removed alpha change from random color.

--- SecondSwiftProject/Model/ColorTools.swift
+++ SecondSwiftProject/Model/ColorTools.swift
@@ -16,8 +16,7 @@ public class ColorTools
         let redAmount = CGFloat (drand48())
         let greenAmount = CGFloat (drand48())
         let blueAmount = CGFloat (drand48())
-        let alphaAmount = CGFloat (drand48())
-        newColor = UIColor(red: redAmount, green: greenAmount, blue: blueAmount, alpha: alphaAmount)
+        newColor = UIColor(red: redAmount, green: greenAmount, blue: blueAmount, alpha: CGFloat (1.0))
         
         return newColor
     }
