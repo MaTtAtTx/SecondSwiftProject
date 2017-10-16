c8bf4cb50371b1f75f46020477d1e4abd65f4057
mattitude17 <31513806+mattitude17@users.noreply.github.com>
2017-10-16 09:11:45 -0600

    Added another view with buttons that go back to previous screens and also change to a random color when the screen loads.

--- SecondSwiftProject/Controller/SecondViewController.swift
+++ SecondSwiftProject/Controller/SecondViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 public class SecondViewController : UIViewController
 {
     @IBOutlet weak var homeButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
     lazy var colorTool : ColorTools = ColorTools()
     public override func viewDidLoad()
     {
@@ -18,5 +19,7 @@ public class SecondViewController : UIViewController
         view.backgroundColor = colorTool.randomColor()
         homeButton.setTitleColor(colorTool.randomColor(), for: .normal)
         homeButton.backgroundColor = colorTool.randomColor()
+        nextButton.setTitleColor(colorTool.randomColor(), for: .normal)
+        nextButton.backgroundColor = colorTool.randomColor()
     }
 }
---
+++ SecondSwiftProject/Controller/ThirdViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ThirdViewController.swift
+//  SecondSwiftProject
+//
+//  Created by Johnsen, Matthew on 10/16/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController
+{
+    @IBOutlet weak var homeButton: UIButton!
+    @IBOutlet weak var backButton: UIButton!
+    lazy var colorTool: ColorTools = ColorTools()
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        view.backgroundColor = colorTool.randomColor()
+        homeButton.setTitleColor(colorTool.randomColor(), for: .normal)
+        homeButton.backgroundColor = colorTool.randomColor()
+        backButton.setTitleColor(colorTool.randomColor(), for: .normal)
+        backButton.backgroundColor = colorTool.randomColor()
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+}
