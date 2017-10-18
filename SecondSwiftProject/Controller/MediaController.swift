9145d39f13494814effac549d74d70111b5d0058
mattitude17 <31513806+mattitude17@users.noreply.github.com>
2017-10-18 09:18:49 -0600

    The media controller background color now changes when the screen loads.

--- SecondSwiftProject/Controller/MediaController.swift
+++ SecondSwiftProject/Controller/MediaController.swift
@@ -51,6 +51,7 @@ class MediaController: UIViewController
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        view.backgroundColor = color.randomColor()
     }
 
     override func didReceiveMemoryWarning()
