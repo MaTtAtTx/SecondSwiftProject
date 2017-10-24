7a06a55eeaf8bda289186263b29095eb6601937c
mattitude17 <31513806+mattitude17@users.noreply.github.com>
2017-10-24 08:57:01 -0600

    Added the code through AVFoundation to allow the sound to play.

--- SecondSwiftProject/Controller/MediaController.swift
+++ SecondSwiftProject/Controller/MediaController.swift
@@ -57,10 +57,62 @@ class MediaController: UIViewController
     {
         super.viewDidLoad()
         view.backgroundColor = color.randomColor()
+        loadAudioFile()
     }
 
     override func didReceiveMemoryWarning()
     {
         super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func soundPlay()
+    {
+        playMusicFile()
+    }
+    
+    private func playMusicFile() -> Void
+    {
+        if let isPlaying = soundPlayer?.isPlaying
+        {
+            if (isPlaying)
+            {
+                soundPlayer?.pause()
+            }
+            else
+            {
+                soundPlayer?.play()
+            }
+        }
+    }
+    
+    private func loadAudioFile()
+    {
+        if let soundURL = NSDataAsset(name: "SwiftSound")
+        {
+            do
+            {
+                try! AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+                try! AVAudioSession.sharedInstance().setActive(true)
+                
+                try soundPlayer = AVAudioPlayer(data: soundURL.data, fileTypeHint: AVFileType.mp3.rawValue)
+                soundSlider.maximumValue = Float ((soundPlayer?.duration)!)
+                Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: (#selector(self.updateSlider)), userInfo: nil, repeats: true)
+            }
+            catch
+            {
+                print("Audio File Load Error")
+            }
+        }
+    }
+    
+    @objc private func updateSlider() -> Void
+    {
+        soundSlider.value = Float ((soundPlayer?.currentTime)!)
+    }
+    
+    @IBAction func slidingSound(_ sender: UISlider)
+    {
+        let seekTime = Double (soundSlider.value)
+        soundPlayer?.currentTime = seekTime
     }
 }
